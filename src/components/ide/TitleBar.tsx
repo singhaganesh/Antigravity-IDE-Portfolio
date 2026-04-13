f3868b0d7ecbd4019346db4fa1787cab65ab87a0
Ganesh Singh <ganeshsingha741@gmail.com>
2026-04-13 16:36:15 +0530

    Change the serach icon

--- src/components/ide/TitleBar.tsx
+++ src/components/ide/TitleBar.tsx
@@ -79,8 +79,17 @@ const TitleBar = () => {
         </div>
 
         <div className="hidden lg:flex items-center gap-3 mr-4 text-muted border-l border-border-color pl-4">
-          <Search size={14} className="hover:text-white cursor-pointer" onClick={() => setIsCommandPaletteOpen(true)} />
-          <Settings size={14} className="hover:text-white cursor-pointer" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 50 50"
+            className="hover:text-white cursor-pointer transform -scale-x-100 transition-colors"
+            onClick={() => setIsCommandPaletteOpen(true)}
+          >
+            <path fill="currentColor" d="M 21 3 C 11.6 3 4 10.6 4 20 C 4 29.4 11.6 37 21 37 C 24.354553 37 27.47104 36.01984 30.103516 34.347656 L 42.378906 46.621094 L 46.621094 42.378906 L 34.523438 30.279297 C 36.695733 27.423994 38 23.870646 38 20 C 38 10.6 30.4 3 21 3 z M 21 7 C 28.2 7 34 12.8 34 20 C 34 27.2 28.2 33 21 33 C 13.8 33 8 27.2 8 20 C 8 12.8 13.8 7 21 7 z"></path>
+          </svg>
+          <Settings size={20} className="hover:text-white cursor-pointer" />
           <Sparkles size={14} className="text-[#00e5cc] cursor-pointer" />
           <div className="w-6 h-6 rounded-full bg-purple-600 flex items-center justify-center text-[10px] text-white font-bold cursor-pointer">G</div>
         </div>
